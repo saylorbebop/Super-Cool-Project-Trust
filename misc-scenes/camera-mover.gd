84c91076d7403269ce887959722ed589c623d748
camelCase9 <xkingcat@duck.com>
2026-03-03 17:27:02 +0400

    origin point globals made a singleton and some more changes to camera mover

--- misc-scenes/camera-mover.gd
+++ misc-scenes/camera-mover.gd
@@ -1,26 +1,11 @@
-## Add the following children to make it work:
-## |--Areas                              
-## |--|--Area3D_1                             
-## |--|--Area3D_2...     # as many as needed
-## Make sure to set the exported nodes!!
-
+## To use, fill the @export-ed variables properly!
 extends Node3D
 
-@onready var children := get_children()
-var areas_child: Node
+@export var area: Area3D
 @export var dummy_child: Node
-@export var origin_point_globals: Node
 
 func move_camera_now(_area) -> void:
-	origin_point_globals.move_camera_to_dummy(dummy_child)
+	Global.move_camera_to_dummy(dummy_child)
 
 func _ready() -> void:
-	for i in children:
-		print_debug(i.name)
-		match i.name:
-			"Areas":
-				areas_child = i
-	
-	var areas: Array[Node] = areas_child.get_children()
-	for i in areas:
-		i.body_entered.connect(move_camera_now)
+	area.body_entered.connect(move_camera_now)
