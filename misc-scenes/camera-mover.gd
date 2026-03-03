a30c739eaa22c0684f8eb4f59862f146abdd10fb
camelCase9 <151428523+camelCase9@users.noreply.github.com>
2026-03-03 13:35:21 +0400

    Updated camera-mover.gd

--- misc-scenes/camera-mover.gd
+++ misc-scenes/camera-mover.gd
@@ -2,12 +2,13 @@
 ## |--Areas                              
 ## |--|--Area3D_1                             
 ## |--|--Area3D_2...     # as many as needed
-## |--Dummy              # should be a Node3D or smth
+## Make sure to set the exported nodes!!
+
 extends Node3D
 
 @onready var children := get_children()
 var areas_child: Node
-var dummy_child: Node
+@export var dummy_child: Node
 @export var origin_point_globals: Node
 
 func move_camera_now(_area) -> void:
@@ -19,9 +20,7 @@ func _ready() -> void:
 		match i.name:
 			"Areas":
 				areas_child = i
-			"Dummy":
-				dummy_child = i
-	print_debug("Areas child: %s, dummy child: %s" % [areas_child, dummy_child])
+	
 	var areas: Array[Node] = areas_child.get_children()
 	for i in areas:
 		i.body_entered.connect(move_camera_now)
